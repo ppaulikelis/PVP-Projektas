6232cba2e6bf0a7f17b7e63c189cdbb97ad5cad5
ppaulikelis <ppaulikelis@gmail.com>
2022-05-05 01:57:28 +0300

    Finished question creation

--- client/src/components/creator/EditGame.js
+++ client/src/components/creator/EditGame.js
@@ -1,4 +1,11 @@
-import { Container, IconButton, TextField, Typography } from '@mui/material';
+import {
+  Container,
+  FormControlLabel,
+  IconButton,
+  Switch,
+  TextField,
+  Typography
+} from '@mui/material';
 import { Box } from '@mui/system';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
@@ -9,6 +16,9 @@ import AddRoundedIcon from '@mui/icons-material/AddRounded';
 import SaveRoundedIcon from '@mui/icons-material/SaveRounded';
 import { LeftPageTitle } from '../additional/PageTitle';
 import { CustomCard } from '../additional/CustomCard';
+import LocalPrintshopRoundedIcon from '@mui/icons-material/LocalPrintshopRounded';
+import jsPDF from 'jspdf';
+import QRCode from 'qrcode';
 
 export default function EditGame() {
   const { id } = useParams();
@@ -25,6 +35,7 @@ export default function EditGame() {
       setGameDescription(game.description);
       if (game.questions) {
         setQuestions(game.questions);
+        console.log(game.questions);
       }
     };
     getGame();
@@ -34,7 +45,11 @@ export default function EditGame() {
     const newQuestion = {
       id: questions.length,
       question: '',
-      answer: ''
+      answer: '',
+      worth: 0,
+      showHint: false,
+      hint: '',
+      key: ''
     };
     setQuestions([...questions, newQuestion]);
   };
@@ -55,6 +70,7 @@ export default function EditGame() {
     };
     copy[id] = question;
     setQuestions(copy);
+    console.log(questions);
   };
 
   const handleQuestionAnswerChange = (id, value) => {
@@ -62,6 +78,47 @@ export default function EditGame() {
     let question = {
       ...copy[id],
       answer: value
+    };
+    copy[id] = question;
+    setQuestions(copy);
+  };
+
+  const handleQuestionWorthChange = (id, value) => {
+    let copy = [...questions];
+    let question = {
+      ...copy[id],
+      worth: value
+    };
+    copy[id] = question;
+    setQuestions(copy);
+  };
+
+  const handleQuestionShowHintChange = (id, value) => {
+    let copy = [...questions];
+    let question = {
+      ...copy[id],
+      showHint: value
+    };
+    if (value) {
+      const key = generateKey();
+      question = {
+        ...question,
+        key: key
+      };
+    }
+    copy[id] = question;
+    setQuestions(copy);
+  };
+
+  const generateKey = () => {
+    return Math.floor(1000 + Math.random() * 9000).toString();
+  };
+
+  const handleQuestionHintChange = (id, value) => {
+    let copy = [...questions];
+    let question = {
+      ...copy[id],
+      hint: value
     };
     copy[id] = question;
     setQuestions(copy);
@@ -76,6 +133,44 @@ export default function EditGame() {
     };
     await updateDoc(gameDoc, newFields);
   };
+
+  async function createPDF() {
+    try {
+      var doc = new jsPDF();
+      var width = doc.internal.pageSize.getWidth();
+      let count = 0;
+      for (const question of questions) {
+        if (question.showHint) {
+          if (count != 0) {
+            doc.addPage();
+          }
+          const result = await QRCode.toDataURL(question.key);
+          doc.addImage(result, 'png', 20, 100, 175, 175);
+          doc.addImage('/logo_be_fono.png', 5, 10, 59, 42);
+          doc.setFontSize(25);
+          if (question.hint.length > 0) {
+            question.hint.match(/.{1,40}/g).forEach((row, index) => {
+              if (row[0] == ' ') {
+                let str = row;
+                str = str.substring(1);
+                doc.text(20, 65 + index * 10, str);
+              } else {
+                doc.text(20, 65 + index * 10, row);
+              }
+            });
+          }
+          doc.setFontSize(75);
+          doc.text(width / 2, 280, question.key, { align: 'center' });
+          count++;
+        }
+      }
+      if (count > 0) {
+        doc.save('demo.pdf');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <>
@@ -134,25 +229,30 @@ export default function EditGame() {
               handleDelete={handleDeleteQuestion}
               handleQuestionNameChange={handleQuestionNameChange}
               handleQuestionAnswerChange={handleQuestionAnswerChange}
+              handleQuestionWorthChange={handleQuestionWorthChange}
+              handleQuestionShowHintChange={handleQuestionShowHintChange}
+              handleQuestionHintChange={handleQuestionHintChange}
               initialQuestionName={question.question}
               initialQuestionAnswer={question.answer}
+              initialQuestionWorth={question.worth}
+              initialQuestionShowHint={question.showHint}
+              initialQuestionHint={question.hint}
             />
           ))}
 
           <Box display="flex" flexDirection={'column'} alignItems="center">
-            <br/>
+            <br />
             <Typography component={'div'} variant="h7" color="#ffffff">
               Norėdami išsaugoti pakeitimus nepamirškite paspausti žalio mygtuko ekrano dešinėje
             </Typography>
           </Box>
-
         </CustomCard>
         <IconButton
           onClick={handleSumbit}
           sx={{
             position: 'fixed',
             right: 40,
-            bottom: 135,
+            bottom: 230,
             backgroundColor: '#517300',
             '&:hover': {
               filter: 'brightness(85%)',
@@ -162,6 +262,22 @@ export default function EditGame() {
             width: '75px'
           }}>
           <SaveRoundedIcon sx={{ color: '#ffffff', height: '50px', width: '50px' }} />
+        </IconButton>
+        <IconButton
+          onClick={createPDF}
+          sx={{
+            position: 'fixed',
+            right: 40,
+            bottom: 135,
+            backgroundColor: '#55B0D5',
+            '&:hover': {
+              filter: 'brightness(85%)',
+              backgroundColor: '#55B0D5'
+            },
+            height: '75px',
+            width: '75px'
+          }}>
+          <LocalPrintshopRoundedIcon sx={{ color: '#ffffff', height: '50px', width: '50px' }} />
         </IconButton>
         <IconButton
           onClick={handleAddQuestion}
@@ -190,9 +306,25 @@ const QuestionForm = (props) => {
     handleDelete,
     handleQuestionNameChange,
     handleQuestionAnswerChange,
+    handleQuestionWorthChange,
+    handleQuestionShowHintChange,
+    handleQuestionHintChange,
     initialQuestionName,
-    initialQuestionAnswer
+    initialQuestionAnswer,
+    initialQuestionWorth,
+    initialQuestionShowHint,
+    initialQuestionHint
   } = props;
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    setChecked(initialQuestionShowHint);
+  }, []);
+
+  const handleChange = () => {
+    setChecked(event.target.checked);
+    handleQuestionShowHintChange(id, event.target.checked);
+  };
 
   return (
     <CustomCard background={'#55B0D5'}>
@@ -226,35 +358,71 @@ const QuestionForm = (props) => {
             style: { color: 'white' }
           }}
         />
-        <TextField
-          onChange={(event) => handleQuestionAnswerChange(id, event.target.value)}
-          value={initialQuestionAnswer}
-          focused
-          color="info"
-          name="answer"
-          label="Atsakymas"
-          sx={{ mb: 2 }}
-          InputLabelProps={{
-            style: { color: 'white' }
-          }}
-          InputProps={{
-            style: { color: 'white' }
-          }}
+        <Box display="flex">
+          <Box display="flex" flexGrow={10} mr={2}>
+            <TextField
+              fullWidth
+              onChange={(event) => handleQuestionAnswerChange(id, event.target.value)}
+              value={initialQuestionAnswer}
+              focused
+              color="info"
+              name="answer"
+              label="Atsakymas"
+              sx={{ mb: 2 }}
+              InputLabelProps={{
+                style: { color: 'white' }
+              }}
+              InputProps={{
+                style: { color: 'white' }
+              }}
+            />
+          </Box>
+          <Box display="flex" flexGrow={1}>
+            <TextField
+              fullWidth
+              onChange={(event) => handleQuestionWorthChange(id, event.target.value)}
+              value={initialQuestionWorth}
+              focused
+              color="info"
+              name="worth"
+              label="Klausimo vertė"
+              type="number"
+              sx={{ mb: 2 }}
+              InputLabelProps={{
+                style: { color: 'white' }
+              }}
+              InputProps={{
+                style: { color: 'white' }
+              }}
+            />
+          </Box>
+        </Box>
+        <FormControlLabel
+          sx={{ color: 'white' }}
+          control={<Switch color="secondary" checked={checked} onChange={handleChange} />}
+          label="Užuomina, kur rasti klausimą"
         />
-        <TextField
-          focused
-          color="info"
-          name="value"
-          label="Klausimo vertė"
-          type="number"
-          sx={{ mb: 2 }}
-          InputLabelProps={{
-            style: { color: 'white' }
-          }}
-          InputProps={{
-            style: { color: 'white' }
-          }}
-        />
+        {checked && (
+          <>
+            <TextField
+              onChange={(event) => handleQuestionHintChange(id, event.target.value)}
+              value={initialQuestionHint}
+              multiline
+              rows={2}
+              focused
+              color="info"
+              name="hint"
+              label="Užuomina, kur rasti klausimą"
+              sx={{ mb: 2, mt: 2 }}
+              InputLabelProps={{
+                style: { color: 'white' }
+              }}
+              InputProps={{
+                style: { color: 'white' }
+              }}
+            />
+          </>
+        )}
       </Box>
     </CustomCard>
   );
